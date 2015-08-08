bd6d946564ae90d9e2b9b3187d3733a53fa46cf5
Darrell Golliher <darrell@golliher.net>
2015-08-08 10:16:56 -0400

    Added documentation for main package.

--- main.go
+++ main.go
@@ -1,3 +1,9 @@
+// A command line Aquos Remote Control for Sharp brand televisions.
+// Certain television sets made by Sharp Inc. respond to commands over
+// a simple network protocol.   This enables applications to control the
+// TV in the same manner than an IR remote control can.
+//
+// This package implements a CLI interface to those televisions.
 package main
 
 import (
